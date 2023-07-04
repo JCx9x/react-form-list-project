e60b06833746c82f30c1204e1ceacc34974a48b8
JCx9x <javicanoramirez@gmail.com>
2023-07-04 16:22:41 +0200

    Bindeo del título del listado de pacientes

--- src/components/ListadoPacientes.jsx
+++ src/components/ListadoPacientes.jsx
@@ -1,24 +1,46 @@
 import Paciente from "./Paciente";
 
 const ListadoPacientes = ({ pacientes }) => {
-  console.log(pacientes)
+  
+
   return (
     <div className="md:w-1/2 lg:w-3/5 w-full">
-      <h2 className="font-bold text-3xl text-center mt-10 md:mt-0">Listado de pacientes</h2>
-      <p className="text-lg mt-5 text-center"> 
-        Administra tus {' '}
-        <span className="text-indigo-600 font-bold">
-          pacientes y citas
-        </span>
-      </p>
 
-      <div>
-        {pacientes.map((paciente) => (
-          <Paciente 
-          key={paciente.id}
-          paciente={paciente} />
-        ))}
-      </div>
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-bold text-3xl text-center mt-10 md:mt-0">Listado de pacientes</h2>
+          <p className="text-lg mt-5 text-center"> 
+            Administra tus {' '}
+            <span className="text-indigo-600 font-bold">
+              pacientes y citas
+            </span>
+          </p>
+
+          <div>
+            {pacientes.map((paciente) => (
+              <Paciente 
+              key={paciente.id}
+              paciente={paciente} />
+            ))}
+          </div>
+
+        </>
+      ) : (
+        <>
+
+          <h2 className="font-bold text-3xl text-center mt-10 md:mt-0">No hay pacientes</h2>
+          <p className="text-lg mt-5 text-center"> 
+            Comienza a agregar {' '}
+            <span className="text-indigo-600 font-bold">
+              pacientes.
+            </span>
+          </p>
+
+        </>
+      )}
+
+      
+
     </div>
   );
 };
